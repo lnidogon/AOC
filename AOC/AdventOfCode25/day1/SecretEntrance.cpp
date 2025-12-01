0914c4dd9fc18508866924eac4dc6f2687f910bd
lnidogon <lovro.nidogon@gmail.com>
2025-12-01 14:39:05 +0100

    Day 1 part 2

--- AOC/AdventOfCode25/day1/SecretEntrance.cpp
+++ AOC/AdventOfCode25/day1/SecretEntrance.cpp
@@ -12,14 +12,34 @@ void solve_part1(std::vector<int> &v) {
     int s = 50;
     int ans = 0;
     for(auto t : v) {
-        s = (s + t + 100) % 100;
+        s = (s + t%100 + 100) % 100;
         ans += !s;
     }
     std::cout << ans << '\n';
 }
 
+void solve_part2(std::vector<int> &v) {
+    int s = 50;
+    int ans = 0;
+    for(auto t : v) {
+        ans+=std::abs(t/100); 
+        int os = s;
+        s = s + t%100;
+        if(s < 0) {
+            if(os > 0) ans ++;
+            s = (s + 100);
+        } else if(s >= 100) {
+            ans++;
+            s %= 100;
+        } else if(s == 0) if(os) ans++;
+    }
+    std::cout << ans << '\n';
+}
+
+
 int main() {
     std::vector<int> v;
     parse_input(v);
     solve_part1(v);
+    solve_part2(v);
 }
